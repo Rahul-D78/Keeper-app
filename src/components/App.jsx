5c065a7aed9a8ed938d8ebc33a8045584d7526dd
Rahul kumar <rahulparida933@gamil.com>
2021-01-07 13:02:37 +0530

    modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-    return (<h1>HLO THIS IS MY REACT APP</h1>)
+    return (<h1>HLO THIS IS MY REACT APP WITH MODIFIED SYNTAX</h1>)
 }
 
 export default App
